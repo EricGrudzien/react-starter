a8a2a4007462b183afae6079bd1898492664ff69
Eric Grudzien <ericgrudzien@Erics-MacBook-Pro.local>
2017-09-06 20:52:00 -0700

    Update package json for paths to dist folder

--- src/server/server.js
+++ src/server/server.js
@@ -5,17 +5,22 @@ import Express from 'express';
 var app = Express();
 var server;
 
+// __dirname:  /Users/ericgrudzien/Git/Starters/react-starter/src/server
+
 // debug: "/Users/ericgrudzien/Git/Starters/react-starter/src/server/client/styles"
-const PATH_STYLES = path.resolve(__dirname, './client/styles');
+// start: "/Users/ericgrudzien/Git/Starters/react-starter/src/client/styles"
+const PATH_STYLES = path.resolve(__dirname, '../client/styles');
 
 // debug: "/Users/ericgrudzien/Git/Starters/react-starter/src/dist"
-const PATH_DIST = path.resolve(__dirname, '../dist');
+// start: "/Users/ericgrudzien/Git/Starters/react-starter/dist"
+const PATH_DIST = path.resolve(__dirname, '../../dist');
 
 app.use('/styles', Express.static(PATH_STYLES));
 app.use(Express.static(PATH_DIST));
 
 app.get('/', (req, res) => {
   // debug: "/Users/ericgrudzien/Git/Starters/react-starter/src/client/index.html"
+  // start: "/Users/ericgrudzien/Git/Starters/react-starter/src/client/index.html"
   res.sendFile(path.resolve(__dirname, '../client/index.html'));
 });
 
